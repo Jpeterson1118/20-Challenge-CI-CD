5dc0798f291c4c7210073ea5400bfef8fad209a6
Jpeterson <peterson.jacob13@protonmail.com>
2025-05-10 20:20:12 -0500

    server works now

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,10 +3,8 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
-
+    let modelExists = await models[modelName].db.listCollections()
+    
     if (modelExists.length) {
       await db.dropCollection(collectionName);
     }
